0ba38a2ee7b101d6e07f0ed04d6eddce541caa15
victornm <victor.nguyenmau@gmail.com>
2021-08-29 15:56:21 +0700

    Add sql

---
+++ devstack/deployment/intidb/00-create-tables.sql
@@ -0,0 +1,96 @@
+CREATE TABLE IF NOT EXISTS `users`
+(
+    `email`      varchar(255) NOT NULL,
+    `password`   varchar(255) NOT NULL,
+    `first_name` varchar(255) NOT NULL,
+    `last_name`  varchar(255) NOT NULL,
+    PRIMARY KEY (`email`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `admin_users`
+(
+    `email`      varchar(255) NOT NULL,
+    `last_login` datetime     NULL,
+    PRIMARY KEY (`email`),
+    FOREIGN KEY (email) REFERENCES users (email)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `regular_users`
+(
+    `email`        varchar(255) NOT NULL,
+    `birthdate`    date         NULL,
+    `sex`          char(1)      NULL,
+    `current_city` varchar(50),
+    `hometown`     varchar(50),
+    PRIMARY KEY (`email`),
+    FOREIGN KEY (email) REFERENCES users (email)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `interests`
+(
+    `email`    varchar(255) NOT NULL,
+    `interest` varchar(50)  NULL,
+    PRIMARY KEY (`email`),
+    FOREIGN KEY (email) REFERENCES regular_users (email)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `school_types`
+(
+    `type_name` varchar(50) NOT NULL,
+    PRIMARY KEY (`type_name`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `schools`
+(
+    `school_name` varchar(50) NOT NULL,
+    `type`        varchar(50) NULL,
+    PRIMARY KEY (`school_name`),
+    FOREIGN KEY (type) REFERENCES school_types (type_name)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `attends`
+(
+    `email`          varchar(255) NOT NULL,
+    `school_name`    varchar(50)  NOT NULL,
+    `year_graduated` int          NULL,
+    UNIQUE (`email`, `school_name`, `year_graduated`),
+    FOREIGN KEY (email) REFERENCES regular_users (email),
+    FOREIGN KEY (school_name) REFERENCES schools (school_name)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `employers`
+(
+    `employer_name` varchar(50) NOT NULL,
+    PRIMARY KEY (`employer_name`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `employments`
+(
+    `email`         varchar(255) NOT NULL,
+    `employer_name` varchar(50)  NOT NULL,
+    `job_title`     varchar(50)  NOT NULL,
+    UNIQUE (`email`, `employer_name`, `job_title`),
+    FOREIGN KEY (email) REFERENCES regular_users (email),
+    FOREIGN KEY (employer_name) REFERENCES employers (employer_name)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `friendships`
+(
+    `email`          varchar(255) NOT NULL,
+    `friend_email`   varchar(255) NOT NULL,
+    `relationship`   varchar(50)  NOT NULL,
+    `date_connected` datetime     NOT NULL,
+    PRIMARY KEY (`email`, `friend_email`),
+    FOREIGN KEY (email) REFERENCES regular_users (email),
+    FOREIGN KEY (friend_email) REFERENCES regular_users (email)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
--- devstack/deployment/intidb/00-users.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `users`
-(
-    `email`      varchar(255) NOT NULL,
-    `password`   varchar(255) NOT NULL,
-    `first_name` varchar(255) NOT NULL,
-    `last_name`  varchar(255) NOT NULL,
-    PRIMARY KEY (`email`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
